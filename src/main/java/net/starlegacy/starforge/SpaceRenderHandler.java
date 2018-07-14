da994cedd1653269d3a0276ca46bfd4bce089352
MicleBrick <miclebrick@outlook.com>
2018-07-14 10:08:47 -0400

    got the render handler working

--- src/main/java/net/starlegacy/starforge/SpaceRenderHandler.java
+++ src/main/java/net/starlegacy/starforge/SpaceRenderHandler.java
@@ -4,22 +4,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.renderer.vertex.VertexBuffer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.client.IRenderHandler;
-
-import java.util.Random;
 
 import static net.minecraft.client.Minecraft.*;
 
 public class SpaceRenderHandler extends IRenderHandler {
     private World world;
     private Minecraft mc;
-
-    private static final ResourceLocation END_SKY_TEXTURES = new ResourceLocation("textures/environment/end_sky.png");
+    private static final ResourceLocation SPACE_TEXTURES = new ResourceLocation("textures/environment/end_sky.png");
 
     public SpaceRenderHandler(World world) {
         this.world = world;
@@ -28,64 +22,57 @@ public class SpaceRenderHandler extends IRenderHandler {
 
     @Override
     public void render(float partialTicks, WorldClient world, Minecraft mc) {
-        if(true) throw new RuntimeException("Render is being called");
         GlStateManager.disableFog();
         GlStateManager.disableAlpha();
         GlStateManager.enableBlend();
         GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
         RenderHelper.disableStandardItemLighting();
         GlStateManager.depthMask(false);
+        mc.renderEngine.bindTexture(SPACE_TEXTURES);
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
 
-        renderStars(bufferbuilder);
+        for (int k1 = 0; k1 < 6; ++k1)
+        {
+            GlStateManager.pushMatrix();
+//
+            if (k1 == 1)
+            {
+                GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+            }
+
+            if (k1 == 2)
+            {
+                GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+            }
+
+            if (k1 == 3)
+            {
+                GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+            }
+
+            if (k1 == 4)
+            {
+                GlStateManager.rotate(90.0F, 0.0F, 0.0F, 1.0F);
+            }
+
+            if (k1 == 5)
+            {
+                GlStateManager.rotate(-90.0F, 0.0F, 0.0F, 1.0F);
+            }
+
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+            bufferbuilder.pos(-100.0D, -100.0D, -100.0D).tex(0.0D, 0.0D).color(255, 255, 255, 255).endVertex();
+            bufferbuilder.pos(-100.0D, -100.0D, 100.0D).tex(0.0D, 100.0D).color(255, 255, 255, 255).endVertex();
+            bufferbuilder.pos(100.0D, -100.0D, 100.0D).tex(100.0D, 100.0D).color(255, 255, 255, 255).endVertex();
+            bufferbuilder.pos(100.0D, -100.0D, -100.0D).tex(100.0D, 0.0D).color(255, 255, 255, 255).endVertex();
+            tessellator.draw();
+            GlStateManager.popMatrix();
+        }
 
         GlStateManager.depthMask(true);
         GlStateManager.enableTexture2D();
         GlStateManager.enableAlpha();
-    }
-
-    private void renderStars(BufferBuilder bufferBuilderIn) {
-        Random random = new Random(10842L);
-        bufferBuilderIn.begin(7, DefaultVertexFormats.POSITION);
-
-        for (int i = 0; i < 1500; ++i) {
-            double d0 = (double) (random.nextFloat() * 2.0F - 1.0F);
-            double d1 = (double) (random.nextFloat() * 2.0F - 1.0F);
-            double d2 = (double) (random.nextFloat() * 2.0F - 1.0F);
-            double d3 = (double) (0.15F + random.nextFloat() * 0.1F);
-            double d4 = d0 * d0 + d1 * d1 + d2 * d2;
-
-            if (d4 < 1.0D && d4 > 0.01D) {
-                d4 = 1.0D / Math.sqrt(d4);
-                d0 = d0 * d4;
-                d1 = d1 * d4;
-                d2 = d2 * d4;
-                double d5 = d0 * 100.0D;
-                double d6 = d1 * 100.0D;
-                double d7 = d2 * 100.0D;
-                double d8 = Math.atan2(d0, d2);
-                double d9 = Math.sin(d8);
-                double d10 = Math.cos(d8);
-                double d11 = Math.atan2(Math.sqrt(d0 * d0 + d2 * d2), d1);
-                double d12 = Math.sin(d11);
-                double d13 = Math.cos(d11);
-                double d14 = random.nextDouble() * Math.PI * 2.0D;
-                double d15 = Math.sin(d14);
-                double d16 = Math.cos(d14);
-
-                for (int j = 0; j < 4; ++j) {
-                    double d18 = (double) ((j & 2) - 1) * d3;
-                    double d19 = (double) ((j + 1 & 2) - 1) * d3;
-                    double d21 = d18 * d16 - d19 * d15;
-                    double d22 = d19 * d16 + d18 * d15;
-                    double d23 = d21 * d12 + 0.0D * d13;
-                    double d24 = 0.0D * d12 - d21 * d13;
-                    double d25 = d24 * d9 - d22 * d10;
-                    double d26 = d22 * d9 + d24 * d10;
-                    bufferBuilderIn.pos(d5 + d25, d6 + d23, d7 + d26).endVertex();
-                }
-            }
-        }
+        GlStateManager.enableAlpha();
     }
 }
