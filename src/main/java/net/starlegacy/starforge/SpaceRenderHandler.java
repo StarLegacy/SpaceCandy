212b8d908c94b7af3ae6b20254e92ae0dc38ca7d
MicleBrick <miclebrick@outlook.com>
2018-07-14 11:44:08 -0400

    Finished skybox

--- src/main/java/net/starlegacy/starforge/SpaceRenderHandler.java
+++ src/main/java/net/starlegacy/starforge/SpaceRenderHandler.java
@@ -1,5 +1,6 @@
 package net.starlegacy.starforge;
 
+import io.netty.util.internal.MathUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.*;
@@ -7,17 +8,24 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.client.IRenderHandler;
+import org.lwjgl.util.vector.Quaternion;
 
 import static net.minecraft.client.Minecraft.*;
 
 public class SpaceRenderHandler extends IRenderHandler {
     private World world;
     private Minecraft mc;
-    private static final ResourceLocation SPACE_TEXTURES = new ResourceLocation("textures/environment/end_sky.png");
+    private ResourceLocation[] SPACE_TEXTURES = new ResourceLocation[6];
 
     public SpaceRenderHandler(World world) {
         this.world = world;
         this.mc = getMinecraft();
+        SPACE_TEXTURES[0] = new ResourceLocation("starforge", "textures/environment/space_down.png");
+        SPACE_TEXTURES[1] = new ResourceLocation("starforge", "textures/environment/space_front.png");
+        SPACE_TEXTURES[2] = new ResourceLocation("starforge", "textures/environment/space_back.png");
+        SPACE_TEXTURES[3] = new ResourceLocation("starforge", "textures/environment/space_up.png");
+        SPACE_TEXTURES[4] = new ResourceLocation("starforge", "textures/environment/space_left.png");
+        SPACE_TEXTURES[5] = new ResourceLocation("starforge", "textures/environment/space_right.png");
     }
 
     @Override
@@ -28,44 +36,44 @@ public class SpaceRenderHandler extends IRenderHandler {
         GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
         RenderHelper.disableStandardItemLighting();
         GlStateManager.depthMask(false);
-        mc.renderEngine.bindTexture(SPACE_TEXTURES);
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
 
-        for (int k1 = 0; k1 < 6; ++k1)
-        {
+        for (int i = 0; i < 6; ++i) {
+            mc.renderEngine.bindTexture(SPACE_TEXTURES[i]);
             GlStateManager.pushMatrix();
-//
-            if (k1 == 1)
-            {
+
+            if (i == 1) {
                 GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
             }
 
-            if (k1 == 2)
-            {
+            if (i == 2) {
+//                GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
                 GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+                GlStateManager.rotate(180.0f, 0.0F, 1.0F, 0.0F);
             }
 
-            if (k1 == 3)
-            {
+            if (i == 3) {
                 GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
             }
 
-            if (k1 == 4)
-            {
+            if (i == 4) {
+//                GlStateManager.rotate(90.0F, 0.0F, 1.0F, 0.0F);
                 GlStateManager.rotate(90.0F, 0.0F, 0.0F, 1.0F);
+                GlStateManager.rotate(-90.0f, 0.0F, 1.0F, 0.0F);
             }
 
-            if (k1 == 5)
-            {
+            if (i == 5) {
                 GlStateManager.rotate(-90.0F, 0.0F, 0.0F, 1.0F);
+                GlStateManager.rotate(90.0f, 0.0F, 1.0F, 0.0F);
             }
 
             bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
-            bufferbuilder.pos(-100.0D, -100.0D, -100.0D).tex(0.0D, 0.0D).color(255, 255, 255, 255).endVertex();
-            bufferbuilder.pos(-100.0D, -100.0D, 100.0D).tex(0.0D, 100.0D).color(255, 255, 255, 255).endVertex();
-            bufferbuilder.pos(100.0D, -100.0D, 100.0D).tex(100.0D, 100.0D).color(255, 255, 255, 255).endVertex();
-            bufferbuilder.pos(100.0D, -100.0D, -100.0D).tex(100.0D, 0.0D).color(255, 255, 255, 255).endVertex();
+            double size = 100.0D;
+            bufferbuilder.pos(-size, -size, -size).tex(0.0D, 0.0D).color(255, 255, 255, 255).endVertex();
+            bufferbuilder.pos(-size, -size, size).tex(0.0D, 1).color(255, 255, 255, 255).endVertex();
+            bufferbuilder.pos(size, -size, size).tex(1, 1).color(255, 255, 255, 255).endVertex();
+            bufferbuilder.pos(size, -size, -size).tex(1, 0.0D).color(255, 255, 255, 255).endVertex();
             tessellator.draw();
             GlStateManager.popMatrix();
         }
